d0767abbe3b232aec5b24183c2b8db716ea291d9
Koitoyuu <k0itoyuu@outlook.com>
2024-01-29 15:13:24 +0800

    添加上传者昵称

--- src/main/java/master/koitoyuu/lanzou/LanzouDownloader.java
+++ src/main/java/master/koitoyuu/lanzou/LanzouDownloader.java
@@ -6,11 +6,14 @@ import master.koitoyuu.lanzou.utils.HTTPUtils;
 import master.koitoyuu.lanzou.utils.LanzouException;
 import master.koitoyuu.lanzou.utils.StringUtils;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author Koitoyuu
+ * LanzouDownloader类提供了对蓝奏云文件的相关操作
+ * 包括获取文件直链和生成下载链接映射表
+ * 作者：Koitoyuu
  */
 public class LanzouDownloader {
     /**
@@ -19,26 +22,15 @@ public class LanzouDownloader {
      * @param url 蓝奏云文件链接
      * @param password 蓝奏云文件密码
      * @return 蓝奏云下载直链
+     * @throws Exception 网络请求或解析异常
      */
-    public static String getDownloadURL(String url,String password) throws Exception {
-        String urlData = HTTPUtils.doGet(url);
-        if (!urlData.isEmpty()) {
-            String downloadScript = StringUtils.getSubString(StringUtils.replaceBlank(urlData), "functiondown_p(){", "});}");
-            String sign = StringUtils.getSubString(downloadScript, "skdklds='", "';");
-            String nextURL = "https://" + StringUtils.getSubString(url, "https://", "/") + StringUtils.getSubString(downloadScript, "url:'", "',");
-            String params = "action=downprocess&sign=" + sign + "&p=" + password;
-            String nextURLData = HTTPUtils.doPost(nextURL,params,url);
-            JsonObject jsonObject = JsonParser.parseString(nextURLData).getAsJsonObject();
-            if (jsonObject.get("zt").getAsInt() == 1)
-                return jsonObject.get("dom").getAsString() + "/file/" + jsonObject.get("url").getAsString();
-            throw new LanzouException(jsonObject.get("inf").getAsString());
-        }
-        throw new NullPointerException();
+    public static String getDownloadURL(String url, String password) throws Exception {
+        JsonObject fileInfo = getFileInfo(url, password);
+        return fileInfo.get("DownloadURL").getAsString();
     }
 
     /**
      * 根据输入的蓝奏云文件链接和密码生成下载链接的映射表。
-     * 对输入的映射表中的每个URL，根据对应的密码生成下载链接，并将结果存储在新的映射表中返回。
      *
      * @param map 包含蓝奏云文件链接和密码的映射表
      * @return 包含蓝奏云文件链接和对应下载链接的映射表
@@ -56,5 +48,42 @@ public class LanzouDownloader {
         });
 
         return downloadMap;
+    }
+
+    /**
+     * 获取文件信息
+     *
+     * @param url 蓝奏云文件链接
+     * @param password 文件密码
+     * @return 包含文件信息的JsonObject
+     * @throws LanzouException 解析异常
+     * @throws IOException 网络异常
+     * @throws NullPointerException 网路异常
+     */
+    public static JsonObject getFileInfo(String url, String password) throws LanzouException, IOException, NullPointerException {
+        String urlData = HTTPUtils.doGet(url);
+        if (!urlData.isEmpty()) {
+            String downloadScript = StringUtils.getSubString(StringUtils.replaceBlank(urlData), "functiondown_p(){", "});}");
+            String sign = StringUtils.getSubString(downloadScript, "skdklds='", "';");
+            String nextURL = "https://" + StringUtils.getSubString(url, "https://", "/") + StringUtils.getSubString(downloadScript, "url:'", "',");
+            String params = "action=downprocess&sign=" + sign + "&p=" + password;
+            String nextURLData = HTTPUtils.doPost(nextURL, params, url);
+            JsonObject jsonObject = JsonParser.parseString(nextURLData).getAsJsonObject();
+            if (jsonObject.get("zt").getAsInt() == 1) {
+                JsonObject object = new JsonObject();
+                String downloadURL = jsonObject.get("dom").getAsString() + "/file/" + jsonObject.get("url").getAsString();
+                String fileName = jsonObject.get("inf").getAsString();
+                String size = StringUtils.getSubString(urlData, "<meta name=\"description\" content=\"文件大小：", "|\" />").replace(" ", "");
+                String uploader = StringUtils.getSubString(urlData,"<span class=\"user-name\">","</span><span class=\"user-name-txt\">");
+                object.addProperty("DownloadURL", downloadURL);
+                object.addProperty("FileName", fileName);
+                object.addProperty("Size", size);
+                object.addProperty("Uploader",uploader);
+                return object;
+            }
+            throw new LanzouException(jsonObject.get("inf").getAsString());
+        }
+
+        throw new NullPointerException();
     }
 }
--- src/main/java/master/koitoyuu/lanzou/utils/StringUtils.java
+++ src/main/java/master/koitoyuu/lanzou/utils/StringUtils.java
@@ -1,46 +1,32 @@
 package master.koitoyuu.lanzou.utils;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringUtils {
-    public static String toCompleteString(final String[] args, final int start) {
-        if(args.length <= start) return "";
-
-        return String.join(" ", Arrays.copyOfRange(args, start, args.length));
-    }
 
     public static String getSubString(String text, String left, String right) {
-        String result = "";
-        int zLen;
         if (left == null || left.isEmpty()) {
-            zLen = 0;
-        } else {
-            zLen = text.indexOf(left);
-            if (zLen > -1) {
-                zLen += left.length();
-            } else {
-                zLen = 0;
-            }
+            return "";
         }
-        int yLen = text.indexOf(right, zLen);
-        if (yLen < 0 || right.isEmpty()) {
-            yLen = text.length();
+        int startIndex = text.indexOf(left);
+        if (startIndex == -1) {
+            return "";
         }
-        result = text.substring(zLen, yLen);
-        return result;
+        startIndex += left.length();
+        int endIndex = right.isEmpty() ? text.length() : text.indexOf(right, startIndex);
+        if (endIndex == -1) {
+            endIndex = text.length();
+        }
+        return text.substring(startIndex, endIndex);
     }
 
     public static String replaceBlank(String str) {
-        String dest = "";
-        if (str != null) {
-            Pattern p = Pattern.compile("\\s*|\t|\r|\n");
-            Matcher m = p.matcher(str);
-            dest = m.replaceAll("");
-
+        if (str == null) {
+            return "";
         }
-        return dest;
+        Pattern pattern = Pattern.compile("\\s*|\t|\r|\n");
+        Matcher matcher = pattern.matcher(str);
+        return matcher.replaceAll("");
     }
-
 }
